9065805121832c27ee3c162c0dfb587d48e348bb
xoin <Xoin@users.noreply.github.com>
2021-04-07 13:05:32 +0200

    Americans, you win, this time

--- CentralDate.js
+++ CentralDate.js
@@ -3,7 +3,7 @@ Turn this in a class so we can dateformat like a normal language
 */
 
 function Get(jsdate) {
-    const Weekdays=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+    const Weekdays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     const Months=["January","February","March","April","May","June","July","August","September","October","November","December"];
     let workingdate
     if (jsdate == undefined) {
@@ -30,7 +30,7 @@ function Get(jsdate) {
         Year: workingdate.getFullYear(),
         Time: workingdate.getTime(),
         Day: workingdate.getDay(),
-        DayFull: Weekdays[workingdate.getDay()-1],
+        DayFull: Weekdays[workingdate.getDay()],
         MonthFull: Months[workingdate.getMonth()]
     }
     return result;
